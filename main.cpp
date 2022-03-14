4f50f9d8cb5e64a5e01af73f151e6feb4fcafbca
Marco Bertoletti <100340284+MarcoBertoletti@users.noreply.github.com>
2022-03-14 09:35:42 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,10 +3,20 @@ using namespace std;
 
 int main(){
 //Qui vanno definite le variabili
-
+float a=0, b=0, c=0;
+float Triangolo=0, Quadrato=0, Rettangolo=0, Trapezio=0;
 cin >> a >> b >> c;
 
 //Qui va scritto il codice per eseguire i calcoli richiesti
 
+Triangolo= (a*b)/2;
+Quadrato= (a*a);
+Rettangolo= (a*b);
+Trapezio= ((a+b)*c)/2;
+cout <<Triangolo <<endl;
+cout <<Quadrato <<endl;
+cout <<Rettangolo <<endl;
+cout <<Trapezio <<endl;
+return 0;
 
 }
